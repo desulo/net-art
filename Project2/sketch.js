3ad18093348cc8b667ffc14588788be62ef226d6
desulo <144717223+desulo@users.noreply.github.com>
2024-05-14 21:48:45 -0400

    Update sketch.js

--- Project2/sketch.js
+++ Project2/sketch.js
@@ -1,9 +1,21 @@
+let x = 0; 
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
     background(220);
+
+    
     fill(255, 0, 0);
-    ellipse(mouseX, mouseY, 50, 50);
+    ellipse(x, height / 2, 50, 50);
+
+    
+    x += 2; 
+
+    
+    if (x > width + 25) {
+        x = -25; 
+    }
 }
